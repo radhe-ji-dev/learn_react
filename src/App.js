bfe2d52a6ba70b81e08629fb681b6501a021aee5
radhe-ji-dev <therktiwadi@gmail.com>
2025-12-31 12:13:41 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ProfileCard from './tasks/dayOne/props';
+import CountAddition from './tasks/dayOne/states';
 
 function App() {
 	return (
 		<div className='App'>
-			<ProfileCard
-				avatar='https://example.com/avatar.jpg'
-				name='John Doe'
-				bio='A passionate developer.'
-			/>
-
-			<ProfileCard
-				avatar='https://example.com/avatar2.jpg'
-				name='Jane Smith'
-				bio='Loves designing user interfaces.'
-			/>
+			<CountAddition />
 		</div>
 	);
 }
---
+++ src/tasks/dayOne/states.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CountAddition() {
+	const [count, setCount] = React.useState(0);
+
+	return (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={() => setCount(count + 1)}>Add</button>
+		</div>
+	);
+}
+
+export default CountAddition;
+
+//some tricky useState usage
+//interview problems
