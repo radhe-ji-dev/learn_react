be409dbaf30116cc4e9ec889e92c37fc018c27f3
radhe-ji-dev <therktiwadi@gmail.com>
2026-05-06 09:07:36 +0530

    update 1 file and create 1 file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import SimpleComp from './tasks/day_1/simpleComp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+/* path or uske element jo ki ek componnet hoga */
 function App() {
-	return <SimpleComp />;
+	return (
+	)
 }
 
 export default App;
