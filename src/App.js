dd9ac38e818495505f1a71a12754b7a6114853c9
radhe-ji-dev <therktiwadi@gmail.com>
2026-05-17 21:50:11 +0530

    update App.js and nav.js routing basics

--- src/App.js
+++ src/App.js
@@ -7,173 +7,23 @@ import CompanyData from './tasks/hooks/useState/companyData';
 
 import contact from './tasks/Routing/contact';
 import about from './tasks/Routing/about';
-import nav from './tasks/Routing/nav';
+import Nav from './tasks/Routing/nav';
 
-import {
-	broswerRoute,
-	BrowserRouter,
-	Route,
-	Router,
-	routes,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	const companyData = [
-		{
-			companyId: 101,
-			companyName: 'TechNova',
-			location: { city: 'Delhi', country: 'India' },
-			departments: [
-				{
-					deptId: 1,
-					deptName: 'Frontend',
-					manager: { id: 11, name: 'Aman' },
-					employees: [
-						{
-							empId: 1001,
-							name: 'Rahul',
-							age: 25,
-							salary: 45000,
-							skills: ['HTML', 'CSS', 'React'],
-							projects: [
-								{
-									projectId: 'P1',
-									title: 'Ecommerce',
-									completed: true,
-									bugs: 12,
-								},
-								{
-									projectId: 'P2',
-									title: 'Dashboard',
-									completed: false,
-									bugs: 20,
-								},
-							],
-						},
-						{
-							empId: 1002,
-							name: 'Karan',
-							age: 29,
-							salary: 60000,
-							skills: ['React', 'NextJS'],
-							projects: [
-								{
-									projectId: 'P3',
-									title: 'Portfolio',
-									completed: true,
-									bugs: 5,
-								},
-							],
-						},
-					],
-				},
-				{
-					deptId: 2,
-					deptName: 'Backend',
-					manager: { id: 12, name: 'Vikas' },
-					employees: [
-						{
-							empId: 1003,
-							name: 'Mohit',
-							age: 31,
-							salary: 80000,
-							skills: ['Node', 'MongoDB', 'Express'],
-							projects: [
-								{
-									projectId: 'P4',
-									title: 'API System',
-									completed: false,
-									bugs: 35,
-								},
-							],
-						},
-					],
-				},
-			],
-		},
-		{
-			companyId: 102,
-			companyName: 'CodeCraft',
-			location: { city: 'Mumbai', country: 'India' },
-			departments: [
-				{
-					deptId: 3,
-					deptName: 'UI/UX',
-					manager: { id: 13, name: 'Priya' },
-					employees: [
-						{
-							empId: 1004,
-							name: 'Sneha',
-							age: 27,
-							salary: 55000,
-							skills: ['Figma', 'Photoshop'],
-							projects: [
-								{
-									projectId: 'P5',
-									title: 'Bank App',
-									completed: true,
-									bugs: 2,
-								},
-							],
-						},
-					],
-				},
-				{
-					deptId: 4,
-					deptName: 'FullStack',
-					manager: { id: 14, name: 'Arjun' },
-					employees: [
-						{
-							empId: 1005,
-							name: 'Rohit',
-							age: 26,
-							salary: 70000,
-							skills: ['React', 'Node', 'MongoDB'],
-							projects: [
-								{
-									projectId: 'P6',
-									title: 'Food App',
-									completed: false,
-									bugs: 18,
-								},
-								{
-									projectId: 'P7',
-									title: 'Chat App',
-									completed: true,
-									bugs: 4,
-								},
-							],
-						},
-						{
-							empId: 1006,
-							name: 'Neha',
-							age: 24,
-							salary: 48000,
-							skills: ['Vue', 'Firebase'],
-							projects: [
-								{
-									projectId: 'P8',
-									title: 'CRM',
-									completed: false,
-									bugs: 27,
-								},
-							],
-						},
-					],
-				},
-			],
-		},
-	];
-
 	return (
-		<BrowserRouter>
-			<Router>
-				<Route path='/' element={<nav />} />
-				<Route path='/about' element={<about />} />
-				<Route path='/contact' element={<contact />} />
-			</Router>
-		</BrowserRouter>
+		<div>
+			<BrowserRouter>
+				<Nav />
+
+				<Routes>
+					<Route path='/' element={<h1>Home</h1>} />
+					<Route path='/about' element={<about />} />
+					<Route path='/contact' element={<contact />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
-
 export default App;
--- src/tasks/Routing/nav.js
+++ src/tasks/Routing/nav.js
@@ -2,15 +2,32 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import about from './about';
 import contact from './contact';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function nav() {
+export default function Nav() {
+	const navigate = useNavigate();
 	return (
 		<div>
 			<nav>
 				<ul>
-					<link to='/'>Home</link>
-					<link to='/about'>About</link>
-					<link to='/contact'>Contact</link>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+
+					<li>
+						<Link to='/contact'>Contact</Link>
+					</li>
+					<li>
+						<button
+							className='shadow-lg-r bg-amber-100'
+							onClick={() => navigate('/')}>
+							Go Home
+						</button>
+					</li>
 				</ul>
 			</nav>
 		</div>
