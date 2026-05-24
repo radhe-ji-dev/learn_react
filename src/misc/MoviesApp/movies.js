286e06bb39ea8b9996ac2c1239c62d2ed1586cd9
radhe-ji-dev <therktiwadi@gmail.com>
2026-05-24 09:15:24 +0530

    update 1 file and delete 1 file

--- src/misc/MoviesApp/movies.js
+++ src/misc/MoviesApp/movies.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fasearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import MovieCard from './movieCard';
-import SearchBar from './searchBar';
 
 export default function Movies() {
 	const [movies, setMovies] = useState([]);
+
+	const searchHandler = () => {
+		console.log('search btn clicked');
+	};
 
 	useEffect(() => {
 		axios
@@ -24,9 +27,23 @@ export default function Movies() {
 
 	return (
 		<div className='bg-slate-500 justify-evenly'>
-			<div className='justify-justify-content-center'>
-				<SearchBar />
+			<div className='justify-content-center'>
+				<div className='p-2 bg-black'>
+					<input
+						className='p-3 rounded-lg text-white h-14 w-1/2'
+						id='searchbarInput'
+						placeholder='Search Movie'></input>
+					<span className='m-3'>
+						<button
+							className=' h-20 text-3xl bg-blue-400  rounded-lg text-white font-bold  w-1/5'
+							onClick={searchHandler}>
+							<FontAwesomeIcon icon={faSearch} />
+							Search
+						</button>
+					</span>
+				</div>
 			</div>
+			
 			<div className='grid grid-cols-5 p-0 m-0'>
 				{movies.map((movie, index) => (
 					<MovieCard key={`${movie.id}-${index}`} movie={movie} />
